1b29efd83a969ee0381f15fde3678275fbdf5f54
PiotrKopczynski <piotrkopczynski52@gmail.com>
2022-09-01 23:01:49 +0200

    fra stasjonær

--- src/arrays/Arrays2D.java
+++ src/arrays/Arrays2D.java
@@ -1,5 +1,6 @@
 package arrays;
 
+import javax.swing.*;
 import java.util.ArrayList;
 /*
 class Bil {
@@ -10,6 +11,50 @@ class Bil {
  */
 
 public class Arrays2D {
+
+    public static int[][] matrise() {
+        boolean test = true;
+        int rader = 0;
+        int kolonner = 0;
+        while(test) {
+            String innRader = JOptionPane.showInputDialog("Skriv inn antall filmanmeldere: ");
+            String innKolonner = JOptionPane.showInputDialog("Skriv inn antall filmer: ");
+            try {
+                rader = Integer.parseInt(innRader);
+                kolonner = Integer.parseInt(innKolonner);
+                test = false;
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }
+        int[][] array = new int[rader][kolonner];
+        int radCount = 0;
+        int kolonneCount = 0;
+        while(radCount<rader) {
+            kolonneCount = 0;
+            while(kolonneCount<kolonner) {
+                int tall = 0;
+                String innTall = JOptionPane.showInputDialog("Skriv inn score " + (kolonneCount+1) + " til filmanmelder nummer " + (radCount+1)
+                + ". Tallet må være mellom 0 og 9.");
+                try {
+                    tall = Integer.parseInt(innTall);
+                }
+                catch (Exception e) {
+                    System.out.println(e);
+                }
+                if (tall<0 || tall>9) {
+                    JOptionPane.showMessageDialog(null,"Tallet er ikke av riktig type.");
+                }
+                else {
+                    array[radCount][kolonneCount] = tall;
+                    kolonneCount++;
+                }
+            }
+            radCount++;
+            System.out.println(radCount);
+        }
+        return array;
+    }
     public static void main(String[] args) {
         /*
         int[][] tabell = new int[4][4];
@@ -61,6 +106,7 @@ public class Arrays2D {
         biler.clear();
         System.out.println("Lengden på arrayet er nå "+biler.size());
          */
+        /*
         int[] array = {1,2,3,4,5,6,7,8,9};
         ArrayList<Integer> liste = new ArrayList<>();
         for (int i=0;i<array.length;i++) {
@@ -76,5 +122,21 @@ public class Arrays2D {
         for (int element : test) {
             System.out.println(element);
         }
+         */
+        int[][] array = matrise();
+        //double[][] array = {{4.0,6.0,2.0,5.0},{7.0,9.0,4.0,8.0},{6.0,9.0,3.0,7.0}};
+        int rader = array.length;
+        int kolonner = array[0].length;
+        double[] scores = new double[kolonner];
+        for (int i=0;i<rader;i++) {
+            for (int j=0;j<kolonner;j++) {
+                scores[j]+= array[i][j];
+            }
+        }
+        for (int i=0;i<kolonner;i++) {
+            scores[i] = scores[i] / rader;
+            System.out.println(scores[i]);
+        }
+
     }
 }
